3cc99a5a0a41d890a63a57f0fc4a81222004e204
Alex Daniels <alexander-daniels@hotmail.co.uk>
2026-03-11 20:12:01 +0000

    feat: resolve white box UI bug with Material 3 and WindowCompat for Android 15

--- android/app/src/main/java/com/p10racing/app/MainActivity.java
+++ android/app/src/main/java/com/p10racing/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.p10racing.app;
 
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends BridgeActivity {
         // Enable edge-to-edge display for Android 15 (SDK 35+) support
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
+        
+        // Ensure the content can be drawn under the system bars
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
     }
 }
